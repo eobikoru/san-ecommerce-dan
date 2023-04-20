20397b991e4f46b57d44faaa07f88ba0a81ffca4
Daniel Emeka Obikoru <daniel.emeka@blinqpay.io>
2023-04-20 11:10:59 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import { Layout } from "@/components";
+import { StateContext } from "@/context/StateContext";
+export default function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}
